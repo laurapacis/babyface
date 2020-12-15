6cf8cf8050eeb6187174d661112f42d5a444a1f8
limononoil <63930000+limononoil@users.noreply.github.com>
2020-12-15 19:38:54 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+### Skincare Ingredient Checker
+
+[https://laurapacis.github.io/skincare-ingredient-checker/](https://laurapacis.github.io/skincare-ingredient-checker/)
+
+Users can visit the [Skincare Ingredient Checker](https://laurapacis.github.io/skincare-ingredient-checker/) to quickly search for sensitizers and allergens in an ingredient list of skincare products :sparkles:
+
+This project is still in progress.
+
+![desktop-view](img/desktop-view.png)
---
+++ js/script.js
@@ -0,0 +1,65 @@
+window.alert("The following site is for educational purposes only! In case you want to start using a new skincare product please talk to your dematologist first.");
+
+const userInput = document.getElementById('input')
+
+const checkerIngredients = "oxybenzones, avobenzone, octinoxate, homosalate, octisalate, octocrylene, fragrance, hydrogen peroxide, sodium lauryl sulfate, sodium silicate, sodium hydroxide, sodium metabisulfite, sodium sulfite, sodium thioglycolate, SD alcohol 40-2, SD alcohol, sodium bisulfite, sodium borate, sodium carbonate, zinc carbonate, zinc sulfate, petrolatum, petroleum jelly, paraffin oil, mineral oil, white petrolatum, petrolem. oil, paraffinium, dimethicone, methicone, trimethicone, cyclomethicone, amodimethicone, trimethylsilylamodimethicone siloxane, cyclopentasiloxane, polydimethylsiloxane conol, ethyl paraben, nutylparaben, methylparaben, propylparaben, isobutylparaben, isopropylparaben, peg, ppg, isopropylparapen, isobutylparaben, propyl parapen, butylparaben, phthalate, dea, tea, perfume, aroma, fullerenes, micronized zinc oxide, nano zinc oxide, micronized titanium dioxide, micronized quartz silica, acetic acid, acetone, calamine, elecampane, english ivy extract, equisetum arvense, esculin, essential oil, ethanol, eucalyptus oil, eugenia aromatica, eugenol, eugenia caryophyllus, comfrey extract, denatured alcohol, fennel oil, fennel seed extract, ferula galbaniflua, fir needle oil, foeniculum vulgare extract, frankincense extract, galbanum, gardenia florida extract, geraniol, geranium oil, geranium pretense, ginger oil, isopropyl alcohol, jasmine oil, jasminium grandiflorum, jonquil extract, juniper berry, juniperus communis, kathon cg, kava-kava extract, kawa extract, kola nut, lavandin oil, lavandula angustifolia, lavandula officinalis, lavender extract, lavender oil, lemon, lemon balm, lemon juice, lemon oil, lemongrass extract, lemongrass oil, gold, grapefruit oil, parfum";
+
+// string method that transforms string into an array
+const arrOfCheckerIngredients = checkerIngredients.split(', ');
+
+// function that transform the string given by the user into an array
+const handleValue = (value) => {
+    let newValue = value;
+    let valueUserInput = newValue.toLowerCase().split(', ');
+    return valueUserInput
+}
+
+// creates an object with all the ingredients given by the user
+const makelistOfIngredients = (valueUserInput) => {
+    const listOfIngredients = {};
+    for (let i = 0; i < valueUserInput.length; i++) {
+        if (listOfIngredients[valueUserInput[i]] === undefined) {
+            listOfIngredients[valueUserInput[i]] = true;
+        }
+    }
+    return listOfIngredients
+}
+
+// creates an array with the ingredients that match
+const fillResult = (array, listOfIngredients) => {
+    const result = []
+    array.forEach((ingredient) => {
+     if (ingredient in listOfIngredients) {
+         result.push(ingredient);
+     }
+    })
+    return result
+}
+
+// if statement that gives an answer to the user
+const outputForUser = (result) => {
+    if (result != 0) {
+        answer.innerText = (`Unfortunately it contains: ${result}. These ingredients may irritate your skin`);
+    } else {
+        answer.innerText = (`Nice! It seems like all these ingredients are safe even for sensitive skin!`);
+    }
+}
+
+// "main" function that checks if the user input matches our given ingredients 
+const checkIngredients = (userInput, arrOfCheckerIngredients) => {
+    let value = userInput.value
+    let valueUserInput = handleValue(value)
+    const listOfIngredients = makelistOfIngredients(valueUserInput)
+    const result = fillResult(arrOfCheckerIngredients, listOfIngredients)
+    
+    answer.innerText = ""
+
+    outputForUser(result)
+};
+
+// 'click' eventListener that activates the main function
+const button = document.getElementById('button');
+button.addEventListener('click', () => {
+    checkIngredients(userInput, arrOfCheckerIngredients)
+}
+);
